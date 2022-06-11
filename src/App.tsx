33fa415bfb7163f43b768ab7d646d8453ee1ef71
kcalmant <kevin.calmant@ext.soprasteria.com>
2022-06-11 14:16:34 +0200

    dev: add scss variable + default color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.scss'
+import HeaderLogo from '../assets/header-logo.png';
 
 function App() {
 	return (
 		<div className="App">
+			<img className="header-logo" src={HeaderLogo} alt="header logo" />
 		</div>
 	)
 }
