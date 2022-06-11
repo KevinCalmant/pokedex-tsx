2288d220c2b1ed45cf2c0f6e8cbb41423256504a
kcalmant <kevin.calmant@ext.soprasteria.com>
2022-06-11 14:58:00 +0200

    dev: add basic list and card for the pokedx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import './App.scss'
-import HeaderLogo from '../assets/header-logo.png';
+import HeaderLogo from '../assets/header-logo.png'
+import { Route, Routes } from "react-router-dom"
+
+const DexList = React.lazy(() => import('./pages/DexList'))
 
 function App() {
-	return (
-		<div className="App">
-			<img className="header-logo" src={HeaderLogo} alt="header logo" />
-		</div>
-	)
+  return (
+      <div className="App">
+        <div className="m-auto">
+          <img className="header-logo m-auto" src={HeaderLogo} alt="header logo"/>
+          <Routes>
+            <Route path="/" element={<DexList/>}/>
+          </Routes>
+        </div>
+      </div>
+  )
 }
 
 export default App
---
+++ src/components/DexCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DexCard = () => {
+  return (
+      <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-row p-5">
+        <img
+            className="rounded-full w-1/4"
+            src="https://toppng.com/uploads/preview/cara-menggambar-pokemon-api-115632259501lkwncr7ey.png"
+            alt="pokefion"/>
+        <div className="p-5">
+          <h3 className="font-bold">Charmeleon</h3>
+        </div>
+      </div>
+  );
+};
+
+export default DexCard;
